b57fb3c5b26890ca3745bcdefa89559aa24248eb
Ihor Rudniev <rudnievihor@gmail.com>
2022-06-12 16:55:06 +0300

    fix-translate(En)

--- src/components/HomeView/HomeView.js
+++ src/components/HomeView/HomeView.js
@@ -2,21 +2,28 @@ import React from 'react';
 
 const styles = {
   container: {
+    marginTop: '30vh',
     minHeight: 'calc(100vh - 50px)',
-    display: 'flex',
+    // display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
   },
   title: {
-    fontWeight: 500,
+    fontWeight: 700,
     fontSize: 48,
+    textAlign: 'center',
+  },
+  text: {
+    fontWeight: 500,
+    fontSize: 40,
     textAlign: 'center',
   },
 };
 
 const HomeView = () => (
   <div style={styles.container}>
-    <h1 style={styles.title}>Приветствую Вас на сервисе хранения контактов!</h1>
+    <h1 style={styles.title}>Приветствую!</h1>
+    <h2 style={styles.text}> Вы на сервисе хранения контактов!</h2>
   </div>
 );
 
